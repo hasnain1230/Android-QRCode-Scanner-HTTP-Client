2eef6d4fd98767a155c755ed23daaa5a93b5840e
Hasnain <hasnain1230@gmail.com>
2024-02-25 07:06:32 -0500

    Added saved link functionality

--- app/src/main/java/com/jscj/qrcodescanner/savedlinks/SavedLinksUI.kt
+++ app/src/main/java/com/jscj/qrcodescanner/savedlinks/SavedLinksUI.kt
@@ -1,24 +1,36 @@
 package com.jscj.qrcodescanner.savedlinks
 
 import android.widget.Toast
-import androidx.compose.foundation.clickable
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.twotone.Add
+import androidx.compose.material.icons.twotone.ArrowBack
+import androidx.compose.material.icons.twotone.Close
+import androidx.compose.material.icons.twotone.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -26,13 +38,20 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.jscj.qrcodescanner.Helper
+import com.jscj.qrcodescanner.R
+import kotlin.math.abs
 
 class SavedLinksUI(private val savedLinksViewModel: SavedLinksViewModel) {
 
@@ -42,21 +61,62 @@ class SavedLinksUI(private val savedLinksViewModel: SavedLinksViewModel) {
         val context = LocalContext.current
         var showDialog by remember { mutableStateOf(false) }
         var newLink by remember { mutableStateOf("") }
+        var isSelectionMode by remember { mutableStateOf(false) }
+        val selectedLinks = remember { mutableStateListOf<String>() }
+
+        // Handle system back press to exit edit mode
+        BackHandler(isSelectionMode) {
+            isSelectionMode = false
+            selectedLinks.clear()
+        }
 
         Scaffold(
             topBar = {
                 TopAppBar(
                     title = { Text("Saved Links") },
                     navigationIcon = {
-                        IconButton(onClick = onNavigateBack) {
-                            Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                        IconButton(onClick = {
+                            if (isSelectionMode) {
+                                isSelectionMode = false
+                                selectedLinks.clear()
+                            } else {
+                                onNavigateBack()
+                            }
+                        }) {
+                            Icon(Icons.TwoTone.ArrowBack, contentDescription = if (isSelectionMode) "Close" else "Back")
+                        }
+                    },
+                    actions = {
+                        if (isSelectionMode) {
+                            if (selectedLinks.isNotEmpty()) {
+                                IconButton(onClick = {
+                                    // TODO: Implement logic to delete selected links
+                                    savedLinksViewModel.removeLinks(selectedLinks)
+                                    selectedLinks.clear()
+                                }) {
+                                    Icon(painter = painterResource(id = R.drawable.twotone_delete_24), contentDescription = "Delete Selected")
+                                }
+                            } else {
+                                IconButton(onClick = {
+                                    isSelectionMode = false
+                                    selectedLinks.clear()
+                                }) {
+                                    Icon(Icons.TwoTone.Close, contentDescription = "Exit Edit Mode")
+                                }
+                            }
+                        } else {
+                            IconButton(onClick = { isSelectionMode = true }) {
+                                Icon(Icons.TwoTone.Edit, contentDescription = "Select")
+                            }
                         }
                     }
                 )
             },
             floatingActionButton = {
-                FloatingActionButton(onClick = { showDialog = true }) {
-                    Icon(Icons.Filled.Add, contentDescription = "Add Link")
+                if (!isSelectionMode) {
+                    FloatingActionButton(onClick = { showDialog = true }) {
+                        Icon(Icons.TwoTone.Add, contentDescription = "Add Link")
+                    }
                 }
             },
             floatingActionButtonPosition = FabPosition.End
@@ -108,10 +168,18 @@ class SavedLinksUI(private val savedLinksViewModel: SavedLinksViewModel) {
                         ListItem(
                             text = { Text(link) },
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    Helper.openUrl(context, link)
-                                }
+                                .fillMaxWidth(),
+                            onClick = {
+                                Helper.openUrl(context, link)
+                            },
+                            onDelete = {
+                                savedLinksViewModel.removeLink(link)
+                            },
+                            isSelected = link in selectedLinks,
+                            onSelect = { isSelected ->
+                                if (isSelected) selectedLinks.add(link) else selectedLinks.remove(link)
+                            },
+                            isSelectionMode = isSelectionMode
                         )
                         Divider()
                     }
@@ -121,9 +189,79 @@ class SavedLinksUI(private val savedLinksViewModel: SavedLinksViewModel) {
     }
 
     @Composable
-    fun ListItem(text: @Composable () -> Unit, modifier: Modifier) {
-        Box(modifier = modifier.padding(16.dp)) {
-            text()
+    fun ListItem(
+        text: @Composable () -> Unit,
+        modifier: Modifier,
+        onClick: () -> Unit,
+        onDelete: () -> Unit,
+        isSelected: Boolean,
+        onSelect: (Boolean) -> Unit,
+        isSelectionMode: Boolean
+    ) {
+        var showMenu by remember { mutableStateOf(false) }
+        var isPressed by remember { mutableStateOf(false) }
+        var dropDownMenuPosition by remember { mutableStateOf(DpOffset.Zero) }
+
+        Box(modifier = modifier.pointerInput(Unit) {
+            detectTapGestures(
+                onLongPress = { offset ->
+                    val xPos = abs((offset.x.toDp()).value).toDp()
+                    dropDownMenuPosition = DpOffset(xPos, 0.dp)
+                    showMenu = true
+                },
+                onTap = { onClick() },
+                onPress = {
+                    isPressed = true
+                    tryAwaitRelease()
+                    isPressed = false
+                }
+            )
+        }) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .background(if (isPressed) MaterialTheme.colorScheme.inversePrimary else MaterialTheme.colorScheme.surface),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (isSelectionMode) {
+                    Checkbox(
+                        checked = isSelected,
+                        onCheckedChange = { onSelect(it) }
+                    )
+                    Spacer(Modifier.width(8.dp))
+                }
+                text()
+            }
+
+            DropdownMenu(
+                expanded = showMenu,
+                onDismissRequest = {
+                    showMenu = false
+                    isPressed = false
+                },
+                offset = dropDownMenuPosition
+            ) {
+                DropdownMenuItem(
+                    onClick = {
+                        onDelete()
+                        showMenu = false
+                    },
+                    text = {
+                        Spacer(Modifier.width(8.dp))
+                        Text("Delete Link")
+                    },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.twotone_delete_forever_24), // Replace with your delete icon asset
+                            contentDescription = "Delete Link",
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                )
+            }
         }
     }
 }
+
+
--- app/src/main/java/com/jscj/qrcodescanner/savedlinks/SavedLinksViewModel.kt
+++ app/src/main/java/com/jscj/qrcodescanner/savedlinks/SavedLinksViewModel.kt
@@ -7,8 +7,13 @@ import androidx.lifecycle.ViewModel
 
 class SavedLinksViewModel(context: Context): ViewModel() {
     // LiveData to hold the list of saved links
+    private val _sharedPreferences = context.getSharedPreferences("saved_links", Context.MODE_PRIVATE)
     private val _savedLinks = MutableLiveData<List<String>>(emptyList())
     val savedLinks: LiveData<List<String>> = _savedLinks
+
+    init {
+        loadSavedLinks()
+    }
 
     // Function to add a new link
     fun addLink(newLink: String) {
@@ -16,16 +21,37 @@ class SavedLinksViewModel(context: Context): ViewModel() {
             val updatedList = _savedLinks.value.orEmpty() + newLink
             _savedLinks.value = updatedList
             // Save the updated list to persistent storage
+            with(_sharedPreferences.edit()) {
+                putStringSet("saved_links", updatedList.toSet())
+                apply() // This applies the changes to the SharedPreferences
+            }
+        }
+    }
+
+    fun removeLink(link: String) {
+        val updatedList = _savedLinks.value.orEmpty() - link // The minus operator removes the link from the list
+        _savedLinks.value = updatedList
+        with(_sharedPreferences.edit()) {
+            putStringSet("saved_links", updatedList.toSet())
+            apply()
+        }
+    }
+
+    // Function to remove list of links
+    fun removeLinks(links: List<String>) {
+        val updatedList = _savedLinks.value.orEmpty() - links.toSet()
+        _savedLinks.value = updatedList
+        with(_sharedPreferences.edit()) {
+            putStringSet("saved_links", updatedList.toSet())
+            apply()
         }
     }
 
     // Function to load saved links from persistent storage
-    fun loadSavedLinks() {
+    private fun loadSavedLinks() {
         // Load the links from SharedPreferences or another storage solution
         // _savedLinks.value = loadedLinks
-    }
-
-    init {
-        loadSavedLinks()
+        val loadedLinks = _sharedPreferences.getStringSet("saved_links", emptySet())
+        _savedLinks.value = loadedLinks?.toList().orEmpty()
     }
 }
