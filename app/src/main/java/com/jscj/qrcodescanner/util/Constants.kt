b702d941f0f9996fb1ca0591481f4e754aeffe37
Hasnain <hasnain1230@gmail.com>
2024-03-13 05:46:51 -0400

    Fixed some color bugs, updated QR code views

--- app/src/main/java/com/jscj/qrcodescanner/util/Constants.kt
+++ app/src/main/java/com/jscj/qrcodescanner/util/Constants.kt
@@ -6,6 +6,7 @@ class Constants {
     companion object {
         val QR_CODE_BEEP: Int = com.google.zxing.client.android.R.raw.zxing_beep
         val ERROR_BEEP: Int = R.raw.error_sound
+        const val TRANSLUCENT_BACKGROUND: Float = 0.6f
         const val FOCUS_AUTO_CANCEL_SECONDS: Long = 1L
         const val SCAN_AREA_MULTIPLIER: Float = 0.6f
         const val CAMERA_PERMISSION_REQUEST_CODE: Int = 100
